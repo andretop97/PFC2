f9ddbbeba708bf6af34b2b626dd3f22107dacfa0
Andre Luiz Branco Barbosa Araujo <andre@andre.andreNet>
2023-10-22 00:07:15 -0300

    YOLOv8 ok, RT-DETR WIP

--- main.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="UuIfC3Pbl4KjTg2y5Ldn")
-project = rf.workspace("dainius").project("smdcomponents")
-dataset = project.version(6).download("yolov8")
